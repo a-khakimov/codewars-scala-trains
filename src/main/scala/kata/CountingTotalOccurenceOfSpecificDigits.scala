474360dbeb13aa01dc4995e56b7b48bbd30b4e0f
a-khakimov <techlinked@yandex.ru>
2020-01-10 01:33:35 +0500

    kata CountingTotalOccurenceOfSpecificDigits (make it better and faster)

--- src/main/scala/kata/CountingTotalOccurenceOfSpecificDigits.scala
+++ src/main/scala/kata/CountingTotalOccurenceOfSpecificDigits.scala
@@ -1,13 +1,13 @@
+/* https://www.codewars.com/kata/56311e4fdd811616810000ce/
+ */
+
 package kata
 
 object NbrList {
   def countSpecDigits(intsList: List[Int], digitsList: List[Int]): List[(Int, Int)] = {
-    digitsList.zip(digitsList.map(
-      digit => {
-        val d = digit.toString.last
-        if (intsList.isEmpty) 0 else
-          intsList.flatMap(_.toString).count(_ == d)
-      })
-    )
+    digitsList.map(digit => {
+      val d = digit.toString.last
+      (digit, intsList.mkString.count(_ == d))
+    })
   }
 }
